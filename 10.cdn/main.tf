4e46be73f07116492c08927d117c75d8c442d22c
prasad <prasad@gmail.com>
2024-07-02 16:39:27 +0530

    infra

--- 10.cdn/main.tf
+++ 10.cdn/main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_distribution" "web_cdn" {
   origin {
-    domain_name              =  "web-${var.environment}.${var.zone_name}"#web-dev.anuprasad.online
+    domain_name              =  "web-${var.environment}.${var.zone_name}"#web-dev01.anuprasad.online
     origin_id                = "web-${var.environment}.${var.zone_name}"
     custom_origin_config  {
         http_port              = 80 // Required to be set but not used
@@ -12,7 +12,7 @@ resource "aws_cloudfront_distribution" "web_cdn" {
 
   enabled             = true
 
-  aliases = ["web-${var.common_tags.Component}.${var.zone_name}"]#web-cdn.daws78s.online
+  aliases = ["web-${var.common_tags.Component}.${var.zone_name}"]#web-cdn.anuprasad.online
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
