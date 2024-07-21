dbba7114da565210e1487d33b6b2401d3251037e
prasad <prasad@gmail.com>
2024-07-21 17:56:52 +0530

    infra

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "project_name" {
-    default = "expense"
+  default = "expense"
 }
 
 variable "environment" {
@@ -7,21 +7,25 @@ variable "environment" {
 }
 
 variable "common_tags" {
-    default = {
-        Project = "expense"
-        Environment = "dev01"
-        Terraform = "true"
-    }
+  default = {
+    Project     = "expense"
+    Environment = "dev01"
+    Terraform   = "true"
+  }
 }
 
 variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24","10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24","10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24","10.0.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "is_peering_required" {
+  default = true
 }
